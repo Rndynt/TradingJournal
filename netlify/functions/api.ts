56c899d2912210f1b0e3dc40aee3d2213bb4661c
Rendyant Maulana <rendyanta.maulana@gmail.com>
2025-07-04 14:05:32 +0700

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,19 @@
+// netlify/functions/api.ts
+import express, { type Request, Response, NextFunction } from "express";
 import serverless from "serverless-http";
-import app from "../../server/index.ts";
+import { registerRoutes } from "../../server/routes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false)});
+
+// Daftarkan hanya route /api
+registerRoutes(app);
+
+// Error handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  res.status(status).json({ message: err.message || "Internal Server Error" });
+});
 
 export const handler = serverless(app);
