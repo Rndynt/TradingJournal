10a858634329a3caedd9845f0ca2e86ece579647
Rendyant Maulana <rendyanta.maulana@gmail.com>
2025-07-04 14:12:05 +0700

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -5,7 +5,7 @@ import { registerRoutes } from "../../server/routes";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false)});
+app.use(express.urlencoded({ extended: false )});
 
 // Daftarkan hanya route /api
 registerRoutes(app);
