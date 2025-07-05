0654e262632cf0fec3ae29eea55dc1042a8e1fc8
Rendyant Maulana <rendyanta.maulana@gmail.com>
2025-07-06 03:03:01 +0700

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -3,6 +3,7 @@
 import { neon } from "@neondatabase/serverless";
 //import { drizzle } from "drizzle-orm/neon-serverless";
 import { drizzle } from 'drizzle-orm/neon-http';
+import { asc, desc } from 'drizzle-orm';
 
 import {
   trades,
